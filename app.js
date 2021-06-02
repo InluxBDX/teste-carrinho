d2a3538ad1f9e5ab2b33f2ccc6d2b82bda6c42e8
Talles Fernando Silva <talles@kiroBe>
2021-06-02 10:46:15 -0300

    Exclusao arquivos node

--- app.js
+++ app.js
@@ -5,14 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fs = require('fs');
 const cors = require('cors');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,9 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
- 
+
 //_1 - produtos abaixo de 10 reais
 //_2 - produtos acima de 10 reais
 
